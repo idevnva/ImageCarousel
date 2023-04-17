205f82e99365b8beaa0a760a01db8712c1e6b406
idevnva <idevnva@gmail.com>
2023-04-18 00:40:34 +0300

    Initial Commit

---
+++ ImageCarousel/ContentView.swift
@@ -0,0 +1,80 @@
+//
+//  ContentView.swift
+//  ImageCarousel
+//
+//  Created by Владислав Новошинский on 18.04.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var currentIndex = 0
+    @GestureState private var dragOffset: CGFloat = 0
+    private let images = ["image1", "image2", "image3", "image4", "image5"]
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                ZStack {
+                    ForEach(0..<images.count, id: \.self) { index in
+                        Image(images[index])
+                            .frame(width: 300, height: 500)
+                            .cornerRadius(25)
+                            .scaleEffect(currentIndex == index ? 1.2 : 0.8)
+                            .opacity(currentIndex == index ? 1.0 : 0.5)
+                            .offset(x: CGFloat(index - currentIndex) * 300 + dragOffset, y: 0)
+                    }
+                }
+                .gesture(
+                    DragGesture()
+                        .updating($dragOffset, body: { (value, state, transaction) in
+                            state = value.translation.width
+                        })
+                        .onEnded({ value in
+                            let threshold: CGFloat = 50
+                            if value.translation.width > threshold {
+                                withAnimation {
+                                    currentIndex = max(0, currentIndex - 1)
+                                }
+                            } else if value.translation.width < -threshold {
+                                withAnimation {
+                                    currentIndex = min(images.count - 1, currentIndex + 1)
+                                }
+                            }
+                        })
+                )
+            }
+            .navigationBarTitle("Image carousel")
+            .toolbar {
+                ToolbarItem(placement: .bottomBar) {
+                    HStack {
+                        Button {
+                            withAnimation {
+                                currentIndex = max(0, currentIndex - 1)
+                            }
+                        } label: {
+                            Image(systemName: "arrow.left")
+                                .font(.title)
+                        }
+                        Spacer()
+                        Button {
+                            withAnimation {
+                                currentIndex = min(images.count - 1, currentIndex + 1)
+                            }
+                        } label: {
+                            Image(systemName: "arrow.right")
+                                .font(.title)
+                        }
+                    }
+                    .padding(.horizontal, 50)
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ImageCarousel/ImageCarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageCarouselApp.swift
+//  ImageCarousel
+//
+//  Created by Владислав Новошинский on 18.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct ImageCarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
